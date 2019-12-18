12d35d3b23b2b977b2cbf84a874c21e75599f6a6
jlundgren77 <jlundgren77@gmail.com>
2019-12-18 08:26:31 -0500

    adjusted column gaps for small screens

--- src/components/EventGrid.js
+++ src/components/EventGrid.js
@@ -8,10 +8,18 @@ const Events = styled.section`
   column-gap: 140px;
   width: 78%;
   margin: 100px auto 200px auto;
-  @media only screen and (max-width: 768px)  {
-    
-      column-count: 2;
-    
+  @media only screen and (max-width: 768px) {
+    column-count: 2;
+  }
+  @media only screen and (max-width: 665px) {
+    column-gap: 85px;
+  }
+  @media only screen and (max-width: 450px) {
+    column-gap: 60px;
+  }
+  @media only screen and (max-width: 350px) {
+    column-gap: 35px;
+  }
 `;
 const Card = styled.div`
   color: #000;
@@ -40,16 +48,16 @@ const Description = styled.p`
   margin-top: 0;
   margin-bottom: 0;
   @media screen and (max-width: 600px) {
-    font-size: 35px;
+    font-size: 30px;
+  }
+  @media screen and (max-width: 400px) {
+    font-size: 25px;
   }
 `;
 const EventMore = styled.p`
-  .Presented_by_Lorem_Ipsum {
-    font-size: 14px;
-    font-family: 'CaslonPro';
-    color: rgb(19, 19, 19);
-    line-height: 3.429;
-  }
+  font-size: 14px;
+  font-family: 'CaslonPro';
+  color: rgb(19, 19, 19);
 `;
 const Content = styled.div`
   margin-bottom: 50px;
